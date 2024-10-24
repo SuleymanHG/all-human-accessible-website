614cde8a319046aa19bdbd3546bda4d84e82855a
SuleymanHuzeyfe <suleyman.gokgul66@outlook.com>
2024-10-24 10:29:08 +0200

    functies

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,56 @@
 const container = document.querySelector('.grid');
+const apiKey = 'Id7ZnvcbuagCH5xt4V02lWUJAj4kCakcxGrbfEDiROH4zpHyC8DUI0Oa';
+let page = 1;
+const query = 'nature';
+const perPage = 50;
 
-const URL = ' https://api.api-ninjas.com/v1/randomimage?category=nature'
+function loadImages() {
+  const URL = `https://api.pexels.com/v1/search?query=${query}&per_page=${perPage}&page=${page}`;
 
-// get the images
-
-function loadImages(numImages = 10){
-   let i=0;
-    while(i < numImages){
-    fetch(' https://api.api-ninjas.com/v1/randomimage?category=nature')
-    .then(response=>response.json())
-    .then(data=>{
-    // console.log(data.message)
-    const img =  document.createElement('img');
-    img.src = `${data.message}`
-    container.appendChild(img)
-    })
-    i++;
-    }   
+  fetch(URL, {
+    headers: {
+      Authorization: apiKey
     }
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.photos && data.photos.length > 0) {
+        data.photos.forEach(photo => {
+          const img = document.createElement('img');
+          img.src = photo.src.medium;
+          img.alt = photo.photographer;
+          container.appendChild(img);
+        });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching images from Pexels:', error);
+    });
+
+  page++;
+}
 
 loadImages();
 
+window.addEventListener('scroll', () => {
+  if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+    loadImages();
+  }
+});
 
 
-// listen for scroll event and load more images if we reach the bottom of window
-window.addEventListener('scroll',()=>{
-    console.log("scrolled", window.scrollY) //scrolled from top
-    console.log(window.innerHeight) //visible part of screen
-    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
-        loadImages();
-    }
-})
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
+  hamburger.setAttribute('aria-expanded', !expanded);
+});
+
+
+const gridSection = document.querySelector('.grid');
+
+gridSection.addEventListener('click', function() {
+  this.classList.toggle('zoomed'); 
+});
